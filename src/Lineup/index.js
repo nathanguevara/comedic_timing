6a7df9c0811769efe7007fd398f762b5ade8ac31
Nathan <nathan.guevara@gmail.com>
2023-03-30 18:48:55 -0500

    pushing to github repo

--- src/Lineup/index.js
+++ src/Lineup/index.js
@@ -1,36 +1,40 @@
-import { useState } from 'react';
-import './styles.css';
+import { useState } from "react";
+import "./styles.css";
 
 const Lineup = () => {
-    const [lineup, setLineup] = useState([]);
-    
-    function handleSubmit(e) {
-        // Prevent the browser from reloading the page
-        e.preventDefault();
-    
-        // Read the form data
-        const form = e.target;
-        const formData = new FormData(form);
-       // TODO: use setLineup to append input values (use spread operator ...) 
-       // TODO: handle edgecase dont allow blank sumbission
-        // TODO: clear out input after submit
-    //    console.log(lineup)
-       console.log(formData.getAll("name"))
-    //    console.log(lineup.concat(formData.getAll("name")))
-    //    console.log([...lineup, ...formData.getAll("name")])
-        setLineup([...lineup, ...formData.getAll("name")])
+  const [lineup, setLineup] = useState([]);
+
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+    // Prevent blank submissions
+    if (formData.get("name") === null) {
+      return;
     }
-    return (
+    // TODO: handle edgecase dont allow blank sumbission
+    // TODO: clear out input after submit
+    // console.log("this is form data",formData)
+    console.log(formData.get("name"));
+
+    setLineup([...lineup, ...formData.getAll("name")]);
+  }
+  return (
     <div>
-        <form method="post" onSubmit={handleSubmit}>
-            <input type="text" id="fname" name="name"></input>
-            <button>Submit</button>
-        </form>
-        <ul>
-        {lineup.map(comic => <li>{comic}</li>)}
-        </ul>
+      <form method="post" onSubmit={handleSubmit}>
+        <input type="text" id="fname" name="name"></input>
+        <button>Submit</button>
+      </form>
+      <ul>
+        {lineup.map((comic) => (
+          <li>{comic}</li>
+        ))}
+      </ul>
     </div>
-    );
+  );
 };
 
 export default Lineup;
